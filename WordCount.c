b05cdae6c5b54c061bee21dfa0624da8f1837e93
umbrella <1252877133@qq.com>
2021-05-06 22:25:36 +0800

    删除了冗余文件，把代码改成了命令行输入

--- WordCount.c
+++ WordCount.c
@@ -1,16 +1,18 @@
 #include<stdio.h>
 #include<string.h>
 #include<stdlib.h>
-void WordCount(char vare,const char* file_name){//-c统计字符数，-w统计单词数
-
+int main(int   argc,char*  argv[]){
+	char mode;
+	char path[1024]={0};
 	char log[1024]={0};
 	char p;
+	
 	int i=0;//字符数
-	int mode=vare;
+	
 	int j=0;
 	int flag=1;
 	int w_num=0;
-	FILE *fp=fopen(file_name,"r");
+	FILE *fp=fopen(argv[2],"r");
 
 	if(fp==NULL){
 		printf("error");
@@ -22,10 +24,10 @@ void WordCount(char vare,const char* file_name){//-c统计字符数，-w统计单词数
 		log[i]=p;
 		i++;
 	}
-	if(mode==99){//99为C的ASCII码
+	if(strcmp(argv[1],"-c")){//99为C的ASCII码
 		printf("字符数:%d\n",i);
 	}
-	else if(mode=119){//119为w的ASCII码
+	else if(argv[2],"-w"){//119为w的ASCII码
 		for(j=0;log[j]!='\0';j++){
 			if(flag==1){
 				if(log[j]!=' '){
@@ -40,11 +42,6 @@ void WordCount(char vare,const char* file_name){//-c统计字符数，-w统计单词数
 		printf("单词数:%d\n",w_num);
 	}
 	fclose(fp);
-}
-int main(void){
-	char mode;
-	char path[1024]={0};
-	scanf("-%c %s",&mode,&path);
-	WordCount(mode,path);
 	system("pause");
 }
+	
